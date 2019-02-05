da2c33c962c0b65bf92cdce4e1a80a3bc1bd404f
Jim Mac <jimmyenmanuel7@gmail.com>
2019-02-05 12:05:52 -0500

    Add POST /todos route and refactor mongoose

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,13 @@
 var mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
+//It is not necessary to put the following statement, because mongoose works with
+//the native Promise, but you can add your own librrary like blouebird if your wish
+//mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('we are connected');
+})
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -7,23 +7,30 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
+  console.log(`we got ${req.body}`)
+  //check if exist the property
+  if(typeof req.body.text ==='undefined') req.body.text = '';
   var todo = new Todo({
     text: req.body.text
   });
 
   todo.save().then((doc) => {
-    res.send(doc)
+    console.log(`this is the doc ${doc}`)
+    res.send(doc);
   }, (err) => {
+    console.log(`this is an error ${err}`);
     res.status(400).send(err);
 
   });
 });
 
 
-
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = {app};
