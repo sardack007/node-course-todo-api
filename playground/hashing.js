45ccc27aed938531df70c5a63b7fef1e8e26a3d2
Jim Mac <jimmyenmanuel7@gmail.com>
2019-02-09 16:01:12 -0500

    Add Hashing password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,31 @@
 const {SHA256} = require('crypto-js');
-var message = 'I am user numbre 3';
-var hash = SHA256(message).toString();
-console.log(message);
-console.log('hash: ', hash);
 
-var data ={
-  id: 4
-};
+const bcrypt = require('bcryptjs');
 
-var token = {
-  data,
-  hash: SHA256(JSON.stringify(data)).toString()
-};
+var password = 'abc123!';
+
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
+
+var hashedPassword ='$2a$10$WjzokXzIvXt7EP//jFGTe.mL2zti2zHkeL2Vpmc9TGVRj9Pvwkppq';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// var message = 'I am user numbre 3';
+// var hash = SHA256(message).toString();
+// console.log(message);
+// console.log('hash: ', hash);
+
+// var data ={
+//   id: 4
+// };
+//
+// var token = {
+//   data,
+//   hash: SHA256(JSON.stringify(data)).toString()
+// };
