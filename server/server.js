ee3114703f1adab8db1f3110dd3bb020889cece6
Jim Mac <jimmyenmanuel7@gmail.com>
2019-02-09 11:18:32 -0500

    Ad GET /users/me and authentication middlewar

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 var port = process.env.PORT;
@@ -120,6 +121,34 @@ app.post('/users', (req, res) => {
   });
 });
 
+// var authenticate = (req, res ,next) => {
+//   var token = req.header('x-auth');
+//
+//   User.findByToken(token).then((user) => {
+//     if (!user){
+//       return Promise.reject();
+//     }
+//     req.user = user;
+//     req.token =token;
+//     next();
+//   }).catch((e) => {
+//     res.status(401).send();
+//   });
+// };
+
+app.get('/users/me',authenticate, (req, res) => {
+  res.send(req.user);
+  // var token = req.header('x-auth');
+  // User.findByToken(token).then((user) => {
+  //   if (!user){
+  //     return Promise.reject();
+  //   }
+  //   res.send(user);
+  // }).catch((e) => {
+  //   res.status(401).send();
+  // });
+
+});
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
