df70d31c15ccca5b939cf202bcd083944c6e721c
Jim Mac <jimmyenmanuel7@gmail.com>
2019-02-14 15:05:15 -0500

    add DELETE /users/me/token

--- server/models/user.js
+++ server/models/user.js
@@ -55,6 +55,17 @@ UserSchema.methods.generateAuthToken = function() {
   });
 };
 
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+  return user.update({
+    $pull: {
+      tokens: {
+        token: token
+      }
+    }
+  });
+}
+
 UserSchema.statics.findByToken = function(token) {
   var User = this;
   var decoded;
@@ -68,7 +79,7 @@ UserSchema.statics.findByToken = function(token) {
 
     return Promise.reject();
   }
-  
+
   return User.findOne({
     '_id': decoded._id,
     'tokens.token': token,
