0553a075106ee3f43a94d7f9c9a7db77b1d64e47
Jim Mac <jimmyenmanuel7@gmail.com>
2019-02-14 12:48:41 -0500

    add POST /users/login

--- server/models/user.js
+++ server/models/user.js
@@ -15,7 +15,7 @@ var UserSchema = new mongoose.Schema({
       validator: (value) => {
         return validator.isEmail(value);
       },
-      nessage: '{VALUE} is not a valid email '
+      message: '{VALUE} is not a valid email '
     }
   },
     password: {
@@ -65,15 +65,37 @@ UserSchema.statics.findByToken = function(token) {
     // return new Promise((resolve, reject) => {
     //   reject();
     // });
+
     return Promise.reject();
   }
-
+  
   return User.findOne({
     '_id': decoded._id,
     'tokens.token': token,
     'tokens.access': 'auth'
   });
 };
+
+UserSchema.statics.findByCredentials = function (email, password){
+  var User = this;
+  return User.findOne({email}).then((user) => {
+    if (!user){
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      // use bcrypt.compare to compare password and user.password
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res){
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+
+  });
+}
 
  UserSchema.pre('save', function(next) {
    var user = this;
